671f48515bc1a14cc94565c924071a0ce8bff060
Alberto Duran <albertoduranfilho@gmail.com>
2026-01-27 09:07:52 -0300

    :sparkles: feat: Adicionando os decorators do Swagger na entidade Usuário e no Controller

--- src/usuario/controllers/usuario.controller.ts
+++ src/usuario/controllers/usuario.controller.ts
@@ -13,8 +13,11 @@ import {
 import { JwtAuthGuard } from '../../auth/guard/jwt-auth.guard';
 import { Usuario } from '../entities/usuario.entity';
 import { UsuarioService } from '../services/usuario.service';
+import { ApiTags, ApiBearerAuth } from '@nestjs/swagger';
 
+@ApiTags('Usuario')
 @Controller('/usuarios')
+@ApiBearerAuth()
 export class UsuarioController {
   constructor(private readonly usuarioService: UsuarioService) {}
 
